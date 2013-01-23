16b7c230ec582079cfe25cb27cd0a9d75fddce0b
Masahiko Sato <masa2sei@gmail.com>
2013-01-23 23:46:26 +0900

    プロジェクトの作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetHyoubanbunseki::Application.config.secret_token = '4bf96c74f517d600f6f55c895236ecab8f1a1cea81965d3c5cc8000184003ecb00be705f2e30e76b63f0f25676fa339c27137f559996d415518c56cbb0659897'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TweetHyoubanbunseki::Application.config.session_store :cookie_store, key: '_tweet_hyoubanbunseki_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TweetHyoubanbunseki::Application.config.session_store :active_record_store
